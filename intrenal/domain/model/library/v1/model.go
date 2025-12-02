3fa2028ba25c5e5a0631dc8de406687278badf21
kiyocy24 <kiyocy24@gmail.com>
2025-12-02 13:38:57 +0900

    library v1

---
+++ intrenal/domain/model/library/v1/model.go
@@ -0,0 +1,48 @@
+package v1
+
+// BookTitle は Book のタイトル
+type BookTitle string
+
+// Book は本
+type Book struct {
+	title BookTitle
+}
+
+// Stock は図書館の在庫
+type Stock struct {
+	book *Book
+}
+
+// StockedBookID は Stock に保存されている Book を識別するための ID
+type StockedBookID string
+
+// StockedBook は Stock に保存されている Book
+type StockedBook struct {
+	id   StockedBookID
+	book *Book
+}
+
+// BorrowedBookID は 借りている本を識別するための ID
+type BorrowedBookID string
+
+// BorrowedBook は書庫から一時的に別の場所で管理されている本
+type BorrowedBook struct {
+	id   BorrowedBookID
+	book *Book
+}
+
+
+// BorrowerID は本を借りる人の識別するための ID
+type BorrowerID string
+
+// Borrower は本を借りる人
+type Borrower struct {
+	id    BorrowerID
+	books []*BorrowedBook
+}
+
+// Borrow は本を借ります
+func (b *Borrower) Borrow(book *BorrowedBook) {}
+
+// Return は借りている本を返します
+func (b *Borrower) Return(book *BorrowedBook) {}
